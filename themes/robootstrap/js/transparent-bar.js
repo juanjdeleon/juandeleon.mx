ed6d06ae5ef0ca12180dbdbfeff551182cce223f
Juan De Leon <newjuan@gmail.com>
2017-08-24 16:32:19 -0500

    Changes to stylesheet and navigation bar.

--- themes/robootstrap/js/transparent-bar.js
+++ themes/robootstrap/js/transparent-bar.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
 	// Enable this effect only at frontpage
 	if ($('body').hasClass('path-frontpage')) {
 		
-		var bannerHeight = $('#frontcover').height();
+		var navbarThreshold = 20;
 		
 		$(window).on('scroll', function(e) {
 			
-			if ($(window).scrollTop() > bannerHeight)
+			if ($(window).scrollTop() > navbarThreshold)
 				$('#navbar').toggleClass('navbar-solid-background', true);
 			else
 				$('#navbar').toggleClass('navbar-solid-background', false);
