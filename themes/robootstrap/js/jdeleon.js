2168daa838fb7c4044607f3db1624836a03c0f5d
Juan De Leon <newjuan@gmail.com>
2017-08-25 16:03:21 -0500

    Added scroll effects.

--- themes/robootstrap/js/jdeleon.js
+++ themes/robootstrap/js/jdeleon.js
@@ -1,13 +1,13 @@
 (function($) {
 $(document).ready(function() {
 
-	// ** Front page effects **
+	// ** Front page **
 	if ($('body').hasClass('path-frontpage')) {
-		
+
+		// Fade effect for navbar		
 		var navbarThreshold = 20;
 		
 		$(window).on('scroll', function(e) {
-			
 			if ($(window).scrollTop() > navbarThreshold)
 				$('#navbar').toggleClass('navbar-solid-background', true);
 			else
@@ -15,16 +15,24 @@ $(document).ready(function() {
 			
 		});
 		
+		// Read story button
+		$('#readstory-link').on('click', function() {
+			$('html,body').animate({
+				scrollTop: $('#mystory-text').offset().top - 75
+			});
+		});
+		
 		// Reveal effects
 		window.sr = ScrollReveal();
 		
 		sr.reveal('#frontcover');
-		sr.reveal('#frontcard', { delay: 300 });
+		sr.reveal('#frontcard', { delay: 300, duration: 1000 });
 		sr.reveal('#mystory-text');
-		sr.reveal('.panel-1st', { origin: 'bottom', delay: 200 });
-		sr.reveal('.panel-2nd', { origin: 'bottom', delay: 500 });
-		sr.reveal('.panel-3rd', { origin: 'bottom', delay: 800 });
-		sr.reveal('.btn-timeline');
+		sr.reveal('.panel-1st', { origin: 'bottom', delay: 200, duration: 1000 });
+		sr.reveal('.panel-2nd', { origin: 'bottom', delay: 500, duration: 1000 });
+		sr.reveal('.panel-3rd', { origin: 'bottom', delay: 800, duration: 1000 });
+		sr.reveal('.btn-timeline', { duration: 800 });
+		sr.reveal('.view-frontpage');
 	}
 
 });
